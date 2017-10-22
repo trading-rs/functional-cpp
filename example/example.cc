99d00e91236f4cb9982ed927d4d78d57860603fb
zjhmale <zjhmale@gmail.com>
2017-10-22 12:33:58 +0800

    Proof on concept on json datatype

--- example/example.cc
+++ example/example.cc
@@ -4,14 +4,29 @@ using namespace fp;
 #include <iostream>
 using namespace std;
 
+#include <json.hpp>
+using json = nlohmann::json;
+
+#include <map>
+using namespace std;
+
 int main(int argc, char** argv) {
   function<int(string)> length = [](string s){ return s.size(); };
   function<int(int)> square = [](int i) { return i*i; };
+  function<long(json)> get_server_time = [](const auto &j) {
+    map<string, long> m = j;
+    return j["serverTime"];
+  };
+  function<long(json)> get_server_time2 = [](const json &j) {
+    return j["serverTime"].get<long>();
+  };
 
   cout << compose(length, square)("cleantha") << endl;
 
   Maybe<string> just_str("cleantha");
   Maybe<string> nothing;
+  json j = json::parse("{\"serverTime\": 1499827319559}");
+  Maybe<json> just_json(j);
 
   cout << "Untouched Maybes:" << endl;
   cout << "\t" << just_str << endl;
@@ -20,4 +35,7 @@ int main(int argc, char** argv) {
   cout << "Maybes fmapped with length:" << endl;
   cout << "\t" << (just_str ^ length) << endl;
   cout << "\t" << (nothing ^ length) << endl;
+
+  cout << (just_json ^ get_server_time) << endl;
+  cout << (just_json ^ get_server_time2) << endl;
 }
--- test/tests.cc
+++ test/tests.cc
@@ -4,17 +4,35 @@
 #include <fp/fp.hpp>
 using namespace fp;
 
-function<int(string)> length = [](string s){ return s.size(); };
-function<int(int)> square = [](int i) { return i*i; };
+#include <json.hpp>
+using json = nlohmann::json;
+
+#include <map>
+using namespace std;
+
+function<int(string)> length = [](const auto &s){ return s.size(); };
+function<int(int)> square = [](const auto &i) { return i * i; };
+function<long(json)> get_server_time = [](const auto &j) {
+  map<string, long> m = j;
+  return j["serverTime"];
+};
+function<long(json)> get_server_time2 = [](const json &j) {
+  return j["serverTime"].get<long>();
+};
 
 TEST_CASE("Test function composition") {
   REQUIRE(compose(length, square)("cleantha") == 64);
 }
 
 TEST_CASE("Test maybe functor") {
+  json j = json::parse("{\"serverTime\": 1499827319559}");
+  Maybe<json> just_json(j);
+
   Maybe<string> just_str("cleantha");
   Maybe<string> nothing;
 
   REQUIRE((just_str ^ length) == (Maybe<int>(8)));
   REQUIRE((nothing ^ length).isNothing());
+  REQUIRE((just_json ^ get_server_time) == (Maybe<long>(1499827319559)));
+  REQUIRE((just_json ^ get_server_time2) == Maybe<long>(1499827319559));
 }
